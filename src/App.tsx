1300a6600fba2c2a8b244c7a97457cfaa5af78b5
Noel Bryant <noel.bryant@savannahinformatics.com>
2025-04-16 19:13:07 +0300

    try solana mainnet rpc

--- src/App.tsx
+++ src/App.tsx
@@ -464,7 +464,8 @@ import getProvider from "./utils/getProvider";
 // Setup Connection
 // const NETWORK = "https://devnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd";
 // const NETWORK = "https://api.devnet.solana.com/";
-const NETWORK = "https://mainnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd"
+// const NETWORK = "https://mainnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd"
+const NETWORK = "https://api.mainnet-beta.solana.com"
 // const NETWORK = "https://api.mainnet-beta.solana.com/";
 const connection = new Connection(NETWORK, "confirmed");
 
--- src/WalletConnectionProvider.jsx
+++ src/WalletConnectionProvider.jsx
@@ -1,34 +1,31 @@
-import React, { useMemo } from 'react';
+import React, { useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
-} from '@solana/wallet-adapter-react';
-import {
-  WalletModalProvider,
-} from '@solana/wallet-adapter-react-ui';
-import {
-  PhantomWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
 
 export const WalletConnectionProvider = ({ children }) => {
   // const network = 'devnet'; // Or 'mainnet-beta' / 'testnet'
-// const network = "https://devnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd";
-const network = "https://mainnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd"
-
+  // const network = "https://devnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd";
+  // const network = "https://mainnet.helius-rpc.com/?api-key=9c13c71d-3088-4fc4-bc03-7c7a270b0bcd"
+  const network = "https://api.mainnet-beta.solana.com";
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(() => [
-    new PhantomWalletAdapter(),
-    // Add more wallets here if needed
-  ], [network]);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      // Add more wallets here if needed
+    ],
+    [network]
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          {children}
-        </WalletModalProvider>
+        <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
